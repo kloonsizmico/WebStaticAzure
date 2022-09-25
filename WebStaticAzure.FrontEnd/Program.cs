c36a5ddf5036c681d2757a4992024609f7f729ec
KlooN SizmicO <wilmardaniel@outlook.com>
2022-09-25 09:01:58 -0500

    config syncfuctions

--- WebStaticAzure.FrontEnd/Program.cs
+++ WebStaticAzure.FrontEnd/Program.cs
@@ -6,6 +6,7 @@ using WebStaticAzure.FrontEnd;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzIyOTMzQDMyMzAyZTMyMmUzMGVncmVRYk1vdk5UTHlDUmc3RzRIamk5VzlRSWpqU0ZKM2FldDlrQXBpb1E9");
 builder.Services.AddSyncfusionBlazor();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
